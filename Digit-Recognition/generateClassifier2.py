f7d6d97b3278e4a6935de3ca149b036a93a2b2fa
MartinJensen37 <Martin.jensen37@gmail.com>
2017-05-19 15:55:58 +0200

    sdf

--- Digit-Recognition/generateClassifier2.py
+++ Digit-Recognition/generateClassifier2.py
@@ -8,6 +8,40 @@ from collections import Counter
 import sys
 
 
+datasets = np.array([])#np.load('./training_data/' + sys.stdin[0][0])
+labels = np.array([])#np.zeros(dataset.size // (28*28), 'int') + int(sys.stdin[0][0])
+
+
 
 for line in sys.stdin:
-    
+
+
+	dataset = np.load('./training_data/' + line)
+	label = np.zeros(dataset.size // (28*28), 'int') + int(line[0])
+
+	datasets = np.concatenate((datasets, dataset))
+	labels = np.concatenate((labels, label))
+
+
+print(labels)
+
+features = dataset.reshape((-1, 28*28))
+
+# Extract the features and labels
+# features = np.array(dataset.data, 'int16') 
+
+# Extract the hog features
+list_hog_fd = []
+for feature in features:
+    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    list_hog_fd.append(fd)
+hog_features = np.array(list_hog_fd, 'float64')
+
+# Create an linear SVM object
+clf = LinearSVC()
+
+# Perform the training
+clf.fit(hog_features, labels)
+
+# Save the classifier
+joblib.dump(clf, "digits_cls1.pkl", compress=3)
